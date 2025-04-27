61ca17214dc4879118284253dd092aa1601e77db
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-04-27 13:42:48 +0530

    sql query edit

---
+++ mysql.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE car_rent_servics;
+
+use  car_rent_servics;
+
+CREATE Table users(
+    emailid VARCHAR(255) not NULL PRIMARY KEY,
+    userid VARCHAR(255) not NULL,
+    password VARCHAR(255) not NULL,
+    role VARCHAR(50) not NULL DEFAULT 'customer',
+);
+CREATE Table login_history(
+    emailid VARCHAR(255) not NULL,
+    password VARCHAR(255) not NULL,
+    login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (emailid) REFERENCES users(emailid),
+
+);
+
+INSERT INTO users (emailid, userid, password, role)
+VALUES ('admin@example.com', 'admin123', 'adminPassword123', 'admin');
