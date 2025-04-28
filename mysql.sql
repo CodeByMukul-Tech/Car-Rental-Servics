573a0b6b325bb8de34d5939e703718dbcd81c3f2
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-04-28 06:25:49 +0530

    ok

--- mysql.sql
+++ mysql.sql
@@ -18,3 +18,16 @@ CREATE Table login_history(
 
 INSERT INTO users (emailid, userid, password, role)
 VALUES ('admin@example.com', 'admin123', 'adminPassword123', 'admin');
+
+
+CREATE TABLE car_details (
+    car_id INT AUTO_INCREMENT PRIMARY KEY,
+    car_model VARCHAR(100) NOT NULL,
+    car_brand VARCHAR(100) NOT NULL,
+    car_price DECIMAL(10, 2) NOT NULL,
+    first_owner_name VARCHAR(100) NOT NULL,
+    first_owner_state VARCHAR(100) NOT NULL,
+    year_of_manufacture INT NOT NULL,
+    registration_number VARCHAR(20) NOT NULL UNIQUE,
+    engine_capacity_cc INT NOT NULL
+);
